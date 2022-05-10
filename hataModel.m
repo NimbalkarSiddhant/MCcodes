b29a0412a6037096e06c59a428386e2e99d7c898
pranavk77 <50962570+pranavk77@users.noreply.github.com>
2022-05-10 23:11:18 +0530

    Update hataModel.m

--- hataModel.m
+++ hataModel.m
@@ -1,50 +1,39 @@
 clc;
-clear all;
 close all;
-
-d= 1:0.01:20;
-
-hm = 5;
-hb1=30;
-hb2=100;
-hb3=200;
-fc = 1000;
-
-ahm = 3.2*(log10(11.75*hm)).^2-4.97;
-
-%for urban cites
-
-L50urban1 = 69.55+26.16*log10(fc)+(44.9-6.55*log10(hb1)*log10(d)-13.82*log10(hb1))-ahm;
-L50urban2 = 69.55+26.16*log10(fc)+(44.9-6.55*log10(hb2)*log10(d)-13.82*log10(hb2))-ahm;
-L50urban3 = 69.55+26.16*log10(fc)+(44.9-6.55*log10(hb3)*log10(d)-13.82*log10(hb3))-ahm;
-
-%for suburban
-
-L50suburban1 = L50urban1-2*(log10(fc/28)).^2-5.4;
-L50suburban2 = L50urban2-2*(log10(fc/28)).^2-5.4;
-L50suburban3 = L50urban3-2*(log10(fc/28)).^2-5.4;
-
-%for rural
-
-L50rural1=L50urban1-4.78*(log10(fc)).^2+18.33*log10(fc)-40.94;
-L50rural2=L50urban2-4.78*(log10(fc)).^2+18.33*log10(fc)-40.94;
-L50rural3=L50urban3-4.78*(log10(fc)).^2+18.33*log10(fc)-40.94;
-
-figure(1);
-plot(d,L50urban1,'r',L50urban2,'--r',L50urban3,':r');
-hold on;
-legend('Large urban hb =30','Large urban hb =100','Large urban hb =200','suburban hb =30','suburban hb =100','suburban hb =200','rural hb =30','rural hb =100','rural hb =200');
-
+clear all;
+d = 1:0.01:20; 
+hm = 5; 
+hb1 = 30;
+hb2 = 100;
+hb3 = 200;
+fc = 1000; 
+% a. For Large Cities
+% fc  >= 400MHz 
+ahm = 3.2*(log10(11.75*hm)).^2 - 4.97;
+% A. Typical Urban
+L50urban1 = 69.55 + 26.16*log10(fc) + (44.9 - 6.55*log10(hb1))*log10(d) - 13.82*log10(hb1) - ahm;
+L50urban2 = 69.55 + 26.16*log10(fc) + (44.9 - 6.55*log10(hb2))*log10(d) - 13.82*log10(hb2) - ahm;
+L50urban3 = 69.55 + 26.16*log10(fc) + (44.9 - 6.55*log10(hb3))*log10(d) - 13.82*log10(hb3) - ahm;
+% B. Typical Suburban
+L50suburban1 = L50urban1 - 2*(log10(fc/28)).^2 - 5.4;
+L50suburban2 = L50urban2 - 2*(log10(fc/28)).^2 - 5.4;
+L50suburban3 = L50urban3 - 2*(log10(fc/28)).^2 - 5.4;
+% C. Typical Rural
+L50rural1 = L50urban1 - 4.78*(log10(fc)).^2 + 18.33*log10(fc) - 40.94;
+L50rural2 = L50urban2 - 4.78*(log10(fc)).^2 + 18.33*log10(fc) - 40.94;
+L50rural3 = L50urban3 - 4.78*(log10(fc)).^2 + 18.33*log10(fc) - 40.94;
+figure(1); 
+plot(d, L50urban1, 'r', d, L50urban2, '--r', d, L50urban3,':r'); 
+hold on; 
+legend('large urban hb=30', 'large urban hb=100', 'large urban hb=200', 'suburban hb=30', 'suburban hb=100', 'suburban hb=200', 'rural hb=30', 'rural hb=100','rural hb=200'); 
 figure(2);
-plot(d,L50suburban1,'b',L50suburban2,'--b',L50suburban3,':b');
-hold on;
-legend('suburban hb =30','suburban hb =100','suburban hb =200');
-
+plot(d, L50suburban1, 'b', d, L50suburban2, '--b', d, L50suburban3, ':b');
+hold on; 
+legend('large urban hb=30', 'large urban hb=100', 'large urban hb=200', 'suburban hb=30', 'suburban hb=100', 'suburban hb=200', 'rural hb=30', 'rural hb=100','rural hb=200'); 
 figure(3);
-plot(d,L50rural1,'g',L50rural2,'--g',L50rural3,':g');
-hold on;
-legend('rural hb =30','rural hb =100','rural hb =200');
-
+plot(d, L50rural1, 'g', d, L50rural2, '--g', d, L50rural3, ':g');
+hold on; 
+legend('large urban hb=30', 'large urban hb=100', 'large urban hb=200', 'suburban hb=30', 'suburban hb=100', 'suburban hb=200', 'rural hb=30', 'rural hb=100','rural hb=200'); 
 grid on;
 xlabel('d [km]');
 ylabel('L [dB]');
